a8a27a532b7cd332b84282bba6bf5a19df73c0e5
Irina <i.s.bondar@mail.ru>
2024-06-22 15:37:08 +0300

    8

--- tests/tests_hw/test_text_box.py
+++ tests/tests_hw/test_text_box.py
@@ -3,6 +3,7 @@ from pages.text_box import TextBox
 name = "test"
 address = "test test"
 
+
 def test_text_box(browser):
     text_box = TextBox(browser)
     text_box.visit()
@@ -10,4 +11,4 @@ def test_text_box(browser):
     text_box.address.send_keys(address)
     text_box.btn_submit.click_force()
     assert text_box.last_name.get_text() == "Name:"+name
-    assert text_box.last_addres.get_text() == "Current Address :"+address
+    assert text_box.last_addres.get_text() == "Current Address :" + address
